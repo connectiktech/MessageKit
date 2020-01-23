4603c94977d2257c24615add98e56104747e99db
Marko Mijatovic <marko.m@connectik.com>
2020-01-23 10:08:20 +0100

    Added InputBarAccessoryView dependency

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MessageKit",
-            dependencies: []),
+            dependencies: ["InputBarAccessoryView"]),
         .testTarget(
             name: "MessageKitTests",
             dependencies: ["MessageKit"]),
